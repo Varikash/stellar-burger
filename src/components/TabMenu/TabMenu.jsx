f6d970bb453159d17e1be05404aa4dc8292c1bea
Nikolay Varikash <n.varicash@gmail.com>
2023-03-16 22:36:08 +0200

    Рефакторинг

--- src/components/TabMenu/TabMenu.jsx
+++ src/components/TabMenu/TabMenu.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import Style from './TabMenu.module.css'
 
 function TabMenu () {
   const [current, setCurrent] = React.useState('one')
   return (
     <div style={{ display: 'flex' }}>
-      <a href='/#Булки'>
+      <a href='/#Булки' className={Style.link}>
         <Tab value="one" active={current === 'one'} onClick={setCurrent}>
           Булки
         </Tab>
       </a>
-      <a href='/#Соусы'>
+      <a href='/#Соусы' className={Style.link}>
         <Tab value="two" active={current === 'two'} onClick={setCurrent}>
           Соусы
         </Tab>
       </a>
-      <a href='/#Начинки'>
+      <a href='/#Начинки'className={Style.link}>
         <Tab value="three" active={current === 'three'} onClick={setCurrent}>
           Начинки
         </Tab>
