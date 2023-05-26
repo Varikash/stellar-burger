1ca65a4b11ebea8be0499697a8f8d04818e556c6
Nikolay Varikash <n.varicash@gmail.com>
2023-05-27 00:19:35 +0300

    feature: auth added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,58 @@
 import { EmailInput, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Link } from 'react-router-dom';
 import Style from './Login.module.css';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { authUser } from '../../services/reducers/handleUserSlice';
+import { Navigate } from 'react-router-dom';
 
 const LoginPage = () => {
+
+  const [form, setValue] = useState({email: '', password: ''});
+  const dispatch = useDispatch();
+  const loading = state => state.user.loading;
+  const logging = state => state.user.loggedIn;
+  const isLoading = useSelector(loading);
+  const isLogged = useSelector(logging);
+
+  const onChange = e => {
+    setValue({ ...form, [e.target.name]: e.target.value });
+  };
+
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch(authUser(form));
+  }
+
   return(
     <section className={`${Style.section} pt-45`}>
-      <form className={Style.container}>
+      <form 
+      className={Style.container}
+      onSubmit={onSubmit}
+      >
         <h1 className={`${Style.title}`}>Вход</h1>
         <EmailInput 
-          name={'email'}
+          name='email'
           isIcon={false}
+          value={form.email}
+          required
+          onChange={onChange}
         />
         <PasswordInput 
-          name={'password'}
+          name='password'
           icon={"ShowIcon"}
+          value={form.password}
+          required
+          onChange={onChange}
         />
-        <Button htmlType="button" type="primary" size="medium" extraClass={`${Style.button} mb-20`}>
-          Войти
-        </Button>
+        {isLoading ? (
+          <div className={Style.spinner}></div>
+        ): (
+          <Button htmlType="submit" type="primary" size="medium" extraClass={`${Style.button} mb-20`}>
+            Войти
+          </Button>
+        )}
         <p className={`${Style.text} text text_type_main-default text_color_inactive`}>
           Вы - новый пользователь?&nbsp;
           <Link to='/register' className={Style.link}>
---
+++ src/services/reducers/handleUserSlice.js
@@ -0,0 +1,66 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { logginUser, registerUser } from "../../utils/apiBackend";
+
+const initialState = {
+  loading: false,
+  success: false,
+  loggedIn: false,
+  registered: false,
+  error: null,
+  user: []
+}
+
+export const createUser = createAsyncThunk(
+  'user/createUser',
+  async(form) => {
+    const response = await registerUser(form);
+    return response
+  }
+)
+
+export const authUser = createAsyncThunk(
+  'user/authUser',
+  async(form) => {
+    const response = await logginUser(form);
+    return response
+  }
+)
+
+export const handleUserSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [authUser.pending]: (state) => {
+      state.loading = true;
+    },
+    [authUser.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.loggedIn = true;
+      state.user = action.payload;
+      localStorage.setItem('accessToken', action.payload.accessToken);
+      localStorage.setItem('refreshToken', action.payload.refreshToken);
+    },
+    [authUser.rejected]: (state, action) => {
+      state.loading = false;
+      state.success = false;
+      state.error = action.error;
+    },
+    [createUser.pending]: (state) => {
+      state.loading = true;
+    },
+    [createUser.fulfilled]: (state) => {
+      state.loading = false;
+      state.success = true;
+      state.registered = true;
+    },
+    [createUser.rejected]: (state, action) => {
+      state.loading = false;
+      state.success = false;
+      state.error = action.error;
+    }
+  }
+})
+
+export default handleUserSlice.reducer;
